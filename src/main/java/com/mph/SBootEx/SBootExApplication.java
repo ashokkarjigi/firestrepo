3b007d20156ba93368ad514ca0b7306778694412
ashok.karjigi <ashok.karjigi@gmail.com>
2020-04-09 17:24:08 +0530

    My forst commit

---
+++ src/main/java/com/mph/SBootEx/SBootExApplication.java
@@ -0,0 +1,13 @@
+package com.mph.SBootEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBootExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SBootExApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mph/SBootEx/SBootExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mph.SBootEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SBootExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
